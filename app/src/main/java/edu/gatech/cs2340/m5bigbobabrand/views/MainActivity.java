72e1367853e70fc3d4d749ce0d01fdb1970faaf3
vinceli1004 <vince.li.ct@gmail.com>
2019-02-16 14:25:31 -0500

    getting difficulty spinner

--- app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/views/MainActivity.java
+++ app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/views/MainActivity.java
@@ -8,10 +8,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Spinner;
 
 import edu.gatech.cs2340.m5bigbobabrand.R;
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner difficultySpinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        difficultySpinner = findViewById(R.id.difficulty_spinner);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
