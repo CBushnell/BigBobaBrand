47ac8722ccd7f77f1f1a8e945589b68af06e33a4
vinceli1004 <vince.li.ct@gmail.com>
2019-02-16 14:09:19 -0500

    commented in useful stuff

--- app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/views/AddPlayerActivity.java
+++ app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/views/AddPlayerActivity.java
@@ -1,4 +1,14 @@
 package edu.gatech.cs2340.m5bigbobabrand.views;
 
-public class AddPlayerActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class AddPlayerActivity extends AppCompatActivity {
+
+    public void onCancelPressed(View view) {
+        onBackPressed();
+    }
 }
+
+//Intent intent = new Intent(this, ViewAllStudentsActivity.class);
+//            startActivity(intent);
