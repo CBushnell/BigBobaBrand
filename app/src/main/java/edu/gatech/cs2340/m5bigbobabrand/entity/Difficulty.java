e432074a0dc628248e07b231d05123138397839f
vinceli1004 <vince.li.ct@gmail.com>
2019-02-21 14:04:01 -0500

    universe update

--- app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/entity/Difficulty.java
+++ app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/entity/Difficulty.java
@@ -1,11 +1,19 @@
 package edu.gatech.cs2340.m5bigbobabrand.entity;
 
+/**
+ * enum for valid difficulties
+ */
 public enum Difficulty {
     BEGINNER("Beginner"), EASY("Easy"), NORMAL("Normal"), HARD("Hard"), IMPOSSIBLE("Impossible");
     private final String difficultyString;
     private Difficulty(String diffString) {
         this.difficultyString = diffString;
     }
+
+    /**
+     *
+     * @return string representation for given difficulty
+     */
     public String getString() {
         return this.difficultyString;
     }
---
+++ app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/entity/Universe.java
@@ -0,0 +1,30 @@
+package edu.gatech.cs2340.m5bigbobabrand.entity;
+
+import java.util.HashMap;
+
+public class Universe {
+    private HashMap<Coordinates, SolarSystem> gameUniverse;
+
+    /**
+     *
+     * @param solarSystem the solar system to add
+     * @return whether or not the solar system was added
+     */
+    public boolean addSolarSystem(SolarSystem solarSystem) {
+        Coordinates currCoordinates = solarSystem.getCoordinate();
+        if (gameUniverse.containsKey(currCoordinates)) {
+            return false;
+        }
+        gameUniverse.put(currCoordinates, solarSystem);
+        return true;
+    }
+
+    /**
+     *
+     * @return the current universe's solar systems
+     */
+    public HashMap<Coordinates, SolarSystem> getSolarSystem() {
+        return this.gameUniverse;
+    }
+
+}
