37e2a44b3c6b05c0f6fa71648cb7ded370c13f8f
Cbushnell <bushnellconnora@gmail.com>
2019-02-21 11:32:57 -0500

    Edited SolarSystem and renamed ResourceLevel

--- app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/entity/ResourceLevel.java
+++ app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/entity/ResourceLevel.java
@@ -1,13 +1,17 @@
 package edu.gatech.cs2340.m5bigbobabrand.entity;
 
-public enum Resources {
+public enum ResourceLevel {
+
     NOSPECIALRESOURCES(0), MINERALRICH(1), MINERALPOOR(2), DESERT(3),
     LOTSOFWATER(4), RICHSOIL(5), POORSOIL(6), RICHFAUNA(7), LIFELESS(8), WEIRDMUSHROOMS(9),
     LOTSOFHERBS(10), ARTISTIC(11), WARLIKE(12);
+
     private final int resourceInt;
+
     private Resources(int resourceInt) {
         this.resourceInt = resourceInt;
     }
+
     public int getResourceInt() {
         return this.resourceInt;
     }
--- app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/entity/SolarSystem.java
+++ app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/entity/SolarSystem.java
@@ -1,9 +1,18 @@
 package edu.gatech.cs2340.m5bigbobabrand.entity;
 
+/**
+ * @author Connor Bushnell
+ * @version 1.0
+ *
+ * A SolarSystem class for holding planets (currently synonymous with planets)
+ */
 public class SolarSystem {
 
     private String name;
-    private int technologyLevel;
+    private TechLevel techLevel;
+    private Resources resourceLevel;
+    private Coordiate coordinate;
+
 
 
 }
