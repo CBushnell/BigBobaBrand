75ed7ece2141299d292a5a11dd36e626ce9cfbc0
Stephanie Chang <chieng.chang@gmail.com>
2019-02-21 11:53:20 -0500

    create Coordinates class for solar systems

---
+++ app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/entity/Coordinates.java
@@ -0,0 +1,56 @@
+package edu.gatech.cs2340.m5bigbobabrand.entity;
+public class Coordinates {
+    private int x;
+    private int y;
+
+    public Coordinates(int x, int y) {
+        // x-coordinate of planet
+        this.x = x;
+        // y-coordinate of planet
+        this.y = y;
+    }
+
+    /**
+     * Set x-coordinate of planet
+     * @param x x-coordinate of planet
+     */
+    public void setX(int x) {
+        if (x >= 0 && x <= 150) {
+            this.x = x;
+        }
+    }
+
+    /**
+     * Set y-coordinate of planet
+     * @param y y-coordinate of planet
+     */
+    public void setY(int y) {
+        if (y >= 0 && y <= 100) {
+            this.y = y;
+        }
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + x + y;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if (this.getClass() != o.getClass()) return false;
+        Coordinates coor = (Coordinates) o;
+        return x == coor.x && y == coor.y;
+    }
+}
