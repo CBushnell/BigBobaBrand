657faa2a8b532d2e76c1edbc7b0f7dcdace3ee8d
Cbushnell <bushnellconnora@gmail.com>
2019-02-22 09:04:05 -0500

    Updated SolarSystem

--- app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/entity/SolarSystem.java
+++ app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/entity/SolarSystem.java
@@ -13,6 +13,18 @@ public class SolarSystem {
     private ResourceLevel resourceLevel;
     private Coordinates coordinate;
 
+    private static final int NAME_MIN_LENGTH = 3;
+    private static final int NAME_MAX_LENGTH = 17;
+
+    //TechLevel probabilities
+    private static final int PREAGRICULTURE_CHANCE = 50;
+    private static final int AGRICULTURE_CHANCE = 60;
+    private static final int MEDIEVAL_CHANCE = 70;
+    private static final int RENAISSANCE_CHANCE = 85;
+    private static final int EARLYINDUSTRIAL_CHANCE = 90;
+    private static final int INDUSTRIAL_CHANCE = 95;
+    private static final int POSTINDUSTRIAL_CHANCE = 98;
+
     public SolarSystem(Coordinates coordinate) {
         this.name = randomName();
         this.techLevel = techLevel;
@@ -21,12 +33,12 @@ public class SolarSystem {
     }
 
     private String randomName() {
-        int length = (int) (Math.random() * 17) + 3;
+        int length = (int) (Math.random() * NAME_MAX_LENGTH) + NAME_MIN_LENGTH;
         String name = "";
         for (int i = 0; i < length; ++i) {
-            String letter = "" + (char) ((Math.random() * 123) + 97);
+            String letter = "" + (char) ((Math.random() * 26) + 97);
             if (i == 0) {
-                letter.toUpperCase();
+                letter = letter.toUpperCase();
             }
             name += letter;
         }
@@ -34,7 +46,21 @@ public class SolarSystem {
     }
 
     private TechLevel randomTech() {
-        return null;
+        int probability = (int) (Math.random() * 101) + 1;
+        if (probability < PREAGRICULTURE_CHANCE) {
+            return TechLevel.PREAGRICULTURE;
+        } else if (probability >= PREAGRICULTURE_CHANCE && probability < AGRICULTURE_CHANCE) {
+
+        } else if (probability >= AGRICULTURE_CHANCE && probability < MEDIEVAL_CHANCE) {
+
+        } else if (probability >= MEDIEVAL_CHANCE && probability < RENAISSANCE_CHANCE) {
+
+        } else if (probability >= RENAISSANCE_CHANCE && probability < EARLYINDUSTRIAL_CHANCE) {
+
+        } else if (probability >= EARLYINDUSTRIAL_CHANCE
+                    && probability < AGRICULTURE_CHANCE) {
+
+        }
     }
 
     private ResourceLevel randomResource() {
@@ -44,11 +70,6 @@ public class SolarSystem {
     @Override
     public int hashCode() {
         return 0;
-    }
-
-    @Override
-    public boolean equals() {
-
     }
 
     public String getName() {
