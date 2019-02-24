803638befb18e74de58889e2318fc35cb9266dda
rmitra34 <rmitra34@gatech.edu>
2019-02-24 12:29:42 -0500

    forgot to add second activity

---
+++ app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/views/SecondActivity.java
@@ -0,0 +1,15 @@
+package edu.gatech.cs2340.m5bigbobabrand.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.gatech.cs2340.m5bigbobabrand.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
