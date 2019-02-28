da2f739e924512bd2521affcbaaf533c6ed9e8d2
cgccomeau <cgccomeau@gatech.edu>
2019-02-28 11:35:55 -0500

    adding IE values

--- app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/entity/IE.java
+++ app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/entity/IE.java
@@ -1,6 +1,21 @@
 package edu.gatech.cs2340.m5bigbobabrand.entity;
 
 public enum IE {
+    DROUGHT("DROUGHT"),
+    COLD("COLD"),
+    CROPFAIL("CROP-FAIL"),
+    WAR("WAR"),
+    BOREDOM("BOREDOM"),
+    PLAGUE("PLAGUE"),
+    LACKOFWORK("LACK-OF-WORK"),
+    RETIREMENT("RETIREMENT"),
+    SNOWINATLANTA("SNOW-IN-ATLANTA!"),
+    SADBOIHOURS("SAD-BOI-HOURS");
 
+    private String IEname;
+
+    private IE(String IEname) {
+        this.IEname = IEname;
+    }
 
 }
