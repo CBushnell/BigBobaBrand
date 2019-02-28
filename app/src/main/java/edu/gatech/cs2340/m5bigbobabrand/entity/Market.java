4f011f92f67fd476810c3b0b72d9e496ec2bdf54
Stephanie Chang <chieng.chang@gmail.com>
2019-02-28 11:54:47 -0500

    update Market

--- app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/entity/Market.java
+++ app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/entity/Market.java
@@ -1,4 +1,21 @@
 package edu.gatech.cs2340.m5bigbobabrand.entity;
 
 public class Market {
+    public static final int NUM_OF_GOODS = 10;
+    private Item[] goods;
+    private IE event;
+    private boolean canSell;
+    private boolean canBuy;
+
+    public Market() {
+        goods = new Item[NUM_OF_GOODS];
+    }
+
+    private void buyGoods(Item good) {
+
+    }
+
+    private void sellGoods(Item good) {
+
+    }
 }
