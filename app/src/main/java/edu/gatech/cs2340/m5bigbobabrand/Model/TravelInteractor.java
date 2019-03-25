0ea22b769263cd4cb733a85d454e436bb2c6e8c1
vinceli1004 <vince.li.ct@gmail.com>
2019-03-25 00:23:05 -0400

    queries user if they want to travel

--- app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/Model/TravelInteractor.java
+++ app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/Model/TravelInteractor.java
@@ -1,5 +1,7 @@
 package edu.gatech.cs2340.m5bigbobabrand.Model;
 
+import android.util.Log;
+
 import edu.gatech.cs2340.m5bigbobabrand.entity.Coordinates;
 import edu.gatech.cs2340.m5bigbobabrand.entity.Player;
 import edu.gatech.cs2340.m5bigbobabrand.entity.SolarSystem;
@@ -15,7 +17,10 @@ public class TravelInteractor {
     public int calculateFuelCosts() {
         Coordinates a = initialSystem.getCoordinates();
         Coordinates b = finalSystem.getCoordinates();
-        int distance = (int) (Math.sqrt(Math.pow(a.getX() + b.getX(), 2) + Math.pow(a.getY() + b.getY(), 2)) + .5);
+        if (a.equals(b)) {
+            return 0;
+        }
+        int distance = (int) (Math.sqrt(Math.pow(a.getX() - b.getX(), 2) + Math.pow(a.getY() - b.getY(), 2)) + .5);
         int fuelCost = (distance / 10); //will add fuel efficiency per ship;
         if (fuelCost == 0) {
             fuelCost += 1;
--- app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/views/TravelActivity.java
+++ app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/views/TravelActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -83,49 +84,76 @@ public class TravelActivity extends AppCompatActivity {
         planetbutton1.setText(solarSystem1.getName());
 
         SolarSystem solarSystem2 = solarSystems[1];
-        planetbutton2.setTranslationX(240 + solarSystem2.getCoordinates().getX() * 8);
+        planetbutton2.setTranslationX(140 + solarSystem2.getCoordinates().getX() * 8);
         planetbutton2.setTranslationY(560 + solarSystem2.getCoordinates().getY() * 8);
         planetbutton2.setText(solarSystem2.getName());
 
         SolarSystem solarSystem3 = solarSystems[2];
-        planetbutton3.setTranslationX(340 + solarSystem3.getCoordinates().getX() * 8);
+        planetbutton3.setTranslationX(140 + solarSystem3.getCoordinates().getX() * 8);
         planetbutton3.setTranslationY(560 + solarSystem3.getCoordinates().getY() * 8);
         planetbutton3.setText(solarSystem3.getName());
 
         SolarSystem solarSystem4 = solarSystems[3];
-        planetbutton4.setTranslationX(440 + solarSystem4.getCoordinates().getX() * 8);
+        planetbutton4.setTranslationX(140 + solarSystem4.getCoordinates().getX() * 8);
         planetbutton4.setTranslationY(560 + solarSystem4.getCoordinates().getY() * 8);
         planetbutton4.setText(solarSystem4.getName());
 
         SolarSystem solarSystem5 = solarSystems[4];
-        planetbutton5.setTranslationX(50 + solarSystem5.getCoordinates().getX() * 8);
+        planetbutton5.setTranslationX(140 + solarSystem5.getCoordinates().getX() * 8);
         planetbutton5.setTranslationY(560 + solarSystem5.getCoordinates().getY() * 8);
         planetbutton5.setText(solarSystem5.getName());
 
         SolarSystem solarSystem6 = solarSystems[5];
-        planetbutton6.setTranslationX(60 + solarSystem6.getCoordinates().getX() * 8);
-        planetbutton6.setTranslationY(660 + solarSystem6.getCoordinates().getY() * 8);
+        planetbutton6.setTranslationX(140 + solarSystem6.getCoordinates().getX() * 8);
+        planetbutton6.setTranslationY(560 + solarSystem6.getCoordinates().getY() * 8);
         planetbutton6.setText(solarSystem6.getName());
 
         SolarSystem solarSystem7 = solarSystems[6];
-        planetbutton7.setTranslationX(70 + solarSystem7.getCoordinates().getX() * 8);
-        planetbutton7.setTranslationY(70 + solarSystem7.getCoordinates().getY() * 8);
+        planetbutton7.setTranslationX(140 + solarSystem7.getCoordinates().getX() * 8);
+        planetbutton7.setTranslationY(560 + solarSystem7.getCoordinates().getY() * 8);
         planetbutton7.setText(solarSystem7.getName());
 
         SolarSystem solarSystem8 = solarSystems[7];
-        planetbutton8.setTranslationX(80 + solarSystem8.getCoordinates().getX() * 8);
-        planetbutton8.setTranslationY(80 + solarSystem8.getCoordinates().getY() * 8);
+        planetbutton8.setTranslationX(140 + solarSystem8.getCoordinates().getX() * 8);
+        planetbutton8.setTranslationY(560 + solarSystem8.getCoordinates().getY() * 8);
         planetbutton8.setText(solarSystem8.getName());
 
         SolarSystem solarSystem9 = solarSystems[8];
-        planetbutton9.setTranslationX(90 + solarSystem9.getCoordinates().getX() * 9);
-        planetbutton9.setTranslationY(90 + solarSystem9.getCoordinates().getY() * 9);
+        planetbutton9.setTranslationX(140 + solarSystem9.getCoordinates().getX() * 9);
+        planetbutton9.setTranslationY(560 + solarSystem9.getCoordinates().getY() * 9);
         planetbutton9.setText(solarSystem9.getName());
 
         SolarSystem solarSystem10 = solarSystems[9];
-        planetbutton10.setTranslationX(100 + solarSystem10.getCoordinates().getX() * 10);
-        planetbutton10.setTranslationY(100 + solarSystem10.getCoordinates().getY() * 10);
+        planetbutton10.setTranslationX(140 + solarSystem10.getCoordinates().getX() * 10);
+        planetbutton10.setTranslationY(560 + solarSystem10.getCoordinates().getY() * 10);
         planetbutton10.setText(solarSystem10.getName());
+
+    }
+
+    private void askForTravel(int planetNum) {
+        final int num = planetNum;
+        SolarSystem[] solarSystems = this.universe.getUniverse()
+                .values().toArray(new SolarSystem[0]);
+        TravelInteractor travelCostCalculator = new TravelInteractor(player.getSolarSystem(),
+                solarSystems[planetNum - 1]);
+        int fuelCost = travelCostCalculator.calculateFuelCosts();
+
+        String fuelMessage = "Traveling to planet " + solarSystems[planetNum - 1].getName()
+                + " will cost " + fuelCost + " fuel";
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirm Travel");
+        builder.setMessage(fuelMessage);
+        builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                travelAttempted(num);
+            }
+        });
+
+        builder.setNegativeButton("Cancel", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
 
     }
 
@@ -135,26 +163,25 @@ public class TravelActivity extends AppCompatActivity {
      * @param planetNum index + 1 in the array of the planet the player is attempting to travel to
      */
     private void travelAttempted(int planetNum) {
-        SolarSystem[] solarSystems = this.universe.getUniverse().values().toArray(new SolarSystem[0]);
+        SolarSystem[] solarSystems = this.universe.getUniverse()
+                .values().toArray(new SolarSystem[0]);
+
 
         //if player's current solarsystem is the button clicked, nothing happens
         if (player.getSolarSystem().equals(solarSystems[planetNum - 1])) {
             Toast.makeText(this, "Currently on this planet", Toast.LENGTH_LONG).show();
-            return;
+            onBackPressed();
         }
 
 
         //calculates fuel costs
         TravelInteractor travelCostCalculator = new TravelInteractor(player.getSolarSystem(),
                 solarSystems[planetNum - 1]);
-
         int fuelCost = travelCostCalculator.calculateFuelCosts();
-        String fuelMessage = "Traveling to planet " + solarSystems[planetNum - 1].getName()
-                + " will cost " + fuelCost + " fuel";
-
-        int subtracted = 0;
 
         //subtracts fuel cost from player's fuel, breaks loop if out of fuel
+        int subtracted = 0;
+
         while (subtracted < fuelCost) {
             if (player.has(Item.FUEL)) {
                 player.loseGood(Item.FUEL);
@@ -187,7 +214,7 @@ public class TravelActivity extends AppCompatActivity {
      * @param view the button clicked
      */
     public void onPlanetButton1Pressed(View view) {
-        travelAttempted(1);
+        askForTravel(1);
     }
     /**
      * On button press, calls a travel attempt between current planet and planet 2
@@ -195,7 +222,7 @@ public class TravelActivity extends AppCompatActivity {
      * @param view the button clicked
      */
     public void onPlanetButton2Pressed(View view) {
-        travelAttempted(2);
+        askForTravel(2);
     }
     /**
      * On button press, calls a travel attempt between current planet and planet 3
@@ -203,7 +230,7 @@ public class TravelActivity extends AppCompatActivity {
      * @param view the button clicked
      */
     public void onPlanetButton3Pressed(View view) {
-        travelAttempted(3);
+        askForTravel(3);
     }
     /**
      * On button press, calls a travel attempt between current planet and planet 4
@@ -211,7 +238,7 @@ public class TravelActivity extends AppCompatActivity {
      * @param view the button clicked
      */
     public void onPlanetButton4Pressed(View view) {
-        travelAttempted(4);
+        askForTravel(4);
     }
     /**
      * On button press, calls a travel attempt between current planet and planet 5
@@ -219,7 +246,7 @@ public class TravelActivity extends AppCompatActivity {
      * @param view the button clicked
      */
     public void onPlanetButton5Pressed(View view) {
-        travelAttempted(5);
+        askForTravel(5);
     }
     /**
      * On button press, calls a travel attempt between current planet and planet 6
@@ -227,7 +254,7 @@ public class TravelActivity extends AppCompatActivity {
      * @param view the button clicked
      */
     public void onPlanetButton6Pressed(View view) {
-        travelAttempted(6);
+        askForTravel(6);
     }
     /**
      * On button press, calls a travel attempt between current planet and planet 7
@@ -235,7 +262,7 @@ public class TravelActivity extends AppCompatActivity {
      * @param view the button clicked
      */
     public void onPlanetButton7Pressed(View view) {
-        travelAttempted(7);
+        askForTravel(7);
     }
     /**
      * On button press, calls a travel attempt between current planet and planet 8
@@ -243,7 +270,7 @@ public class TravelActivity extends AppCompatActivity {
      * @param view the button clicked
      */
     public void onPlanetButton8Pressed(View view) {
-        travelAttempted(8);
+        askForTravel(8);
     }
     /**
      * On button press, calls a travel attempt between current planet and planet 9
@@ -251,7 +278,7 @@ public class TravelActivity extends AppCompatActivity {
      * @param view the button clicked
      */
     public void onPlanetButton9Pressed(View view) {
-        travelAttempted(9);
+        askForTravel(9);
     }
     /**
      * On button press, calls a travel attempt between current planet and planet 10
@@ -259,7 +286,7 @@ public class TravelActivity extends AppCompatActivity {
      * @param view the button clicked
      */
     public void onPlanetButton10Pressed(View view) {
-        travelAttempted(10);
+        askForTravel(10);
     }
 
 
