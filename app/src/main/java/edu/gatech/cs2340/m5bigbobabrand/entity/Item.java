44bc42306932e400d0cd25b0f2b4e6fcadee2743
cgccomeau <cgccomeau@gatech.edu>
2019-04-04 11:15:48 -0400

    Item class inspection complete

--- app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/entity/Item.java
+++ app/src/main/java/edu/gatech/cs2340/m5bigbobabrand/entity/Item.java
@@ -1,16 +1,23 @@
 package edu.gatech.cs2340.m5bigbobabrand.entity;
 
+/**
+ * enum class for an Item and all of its instance data
+ */
 public enum Item {
     WATER(0,0,2,30,3,4, IE.DROUGHT, ResourceLevel.LOTSOFWATER, ResourceLevel.DESERT, 30, 50,0),
     FURS(0,0,0,250,10,10, IE.COLD,ResourceLevel.RICHFAUNA, ResourceLevel.LIFELESS, 230, 280,1),
     FOOD(1,0,1,100,5,5, IE.CROPFAIL, ResourceLevel.RICHSOIL, ResourceLevel.POORSOIL, 90, 160,2),
     ORE(2,2,3,350,20,10, IE.WAR, ResourceLevel.MINERALRICH, ResourceLevel.MINERALPOOR, 350, 420,3),
     GAMES(3,1,6,250,-10,5, IE.WAR, ResourceLevel.ARTISTIC, ResourceLevel.NEVER, 160, 270,4),
-    FIREARMS(3,1,5,1250,-75,100, IE.BOREDOM, ResourceLevel.WARLIKE, ResourceLevel.NEVER, 600, 1100,5),
-    MEDICINE(4,1,6,650,-20,10, IE.PLAGUE, ResourceLevel.LOTSOFHERBS, ResourceLevel.NEVER, 400, 700,6),
+    FIREARMS(3,1,5,1250,-75,100,
+            IE.BOREDOM, ResourceLevel.WARLIKE, ResourceLevel.NEVER, 600, 1100,5),
+    MEDICINE(4,1,6,650,-20,10,
+            IE.PLAGUE, ResourceLevel.LOTSOFHERBS, ResourceLevel.NEVER, 400, 700,6),
     MACHINES(4,3,5,900,-30,5, IE.LACKOFWORK,ResourceLevel.NEVER, ResourceLevel.NEVER, 600, 800,7),
-    NARCOTICS(5,0,5,3500,-125,150, IE.BOREDOM,ResourceLevel.WEIRDMUSHROOMS, ResourceLevel.NEVER, 2000, 3000,8),
-    ROBOTS(6,4,7,5000,-150,100, IE.LACKOFWORK, ResourceLevel.NEVER, ResourceLevel.NEVER, 3500, 5000,9),
+    NARCOTICS(5,0,5,3500,-125,150,
+            IE.BOREDOM,ResourceLevel.WEIRDMUSHROOMS, ResourceLevel.NEVER, 2000, 3000,8),
+    ROBOTS(6,4,7,5000,-150,100,
+            IE.LACKOFWORK, ResourceLevel.NEVER, ResourceLevel.NEVER, 3500, 5000,9),
     FUEL(0,0,5,6,-1,0,IE.SNOWINATLANTA,ResourceLevel.DESERT,ResourceLevel.MINERALPOOR,1,6,10);
 
 
@@ -44,98 +51,194 @@ public enum Item {
         this.ords = ords;
     }
 
+    /**
+     * getter method for MTLP
+     * @return Item's MTLP
+     */
     public int getMTLP() {
         return MTLP;
     }
 
+    /**
+     * setter method for MTLP
+     * @param MTLP to be set
+     */
     public void setMTLP(int MTLP) {
         this.MTLP = MTLP;
     }
 
+    /**
+     * getter method for MTLU
+     * @return Item's MTLU
+     */
     public int getMTLU() {
         return MTLU;
     }
 
+    /**
+     * setter method for MTLU
+     * @param MTLU to be set
+     */
     public void setMTLU(int MTLU) {
         this.MTLU = MTLU;
     }
 
+    /**
+     * getter method for TTP
+     * @return Item's TTP
+     */
     public int getTTP() {
         return TTP;
     }
 
+    /**
+     * setter method for TTP
+     * @param TTP to be set
+     */
     public void setTTP(int TTP) {
         this.TTP = TTP;
     }
 
+    /**
+     * getter method for Base Price
+     * @return Item's base price
+     */
     public int getBasePrice() {
         return basePrice;
     }
 
+    /**
+     * setter method for base price
+     * @param basePrice to be set
+     */
     public void setBasePrice(int basePrice) {
         this.basePrice = basePrice;
     }
 
+    /**
+     * getter method for IPL
+     * @return Item's IPL
+     */
     public int getIPL() {
         return IPL;
     }
 
+    /**
+     * setter method for IPL
+     * @param IPL to be set
+     */
     public void setIPL(int IPL) {
         this.IPL = IPL;
     }
 
+    /**
+     * getter method for var
+     * @return Item's var
+     */
     public int getVar() {
         return var;
     }
 
+    /**
+     * setter method for var
+     * @param var to be set
+     */
     public void setVar(int var) {
         this.var = var;
     }
 
+    /**
+     * getter method for cat
+     * @return Item's cat
+     */
     public IE getCat() {
         return cat;
     }
 
+    /**
+     * setter method for cat
+     * @param cat to be set
+     */
     public void setCat(IE cat) {
         this.cat = cat;
     }
 
+    /**
+     * getter method for Hap
+     * @return Item's hap
+     */
     public ResourceLevel getHap() {
         return hap;
     }
 
+    /**
+     * setter method for hap
+     * @param hap to be set
+     */
     public void setHap(ResourceLevel hap) {
         this.hap = hap;
     }
 
+    /**
+     * getter method for plan
+     * @return Item's plan
+     */
     public ResourceLevel getPlan() {
         return plan;
     }
 
+    /**
+     * setter method for plan
+     * @param plan to be set
+     */
     public void setPlan(ResourceLevel plan) {
         this.plan = plan;
     }
 
+    /**
+     * getter method for MTL
+     * @return Item's MTL
+     */
     public int getMTL() {
         return MTL;
     }
 
+    /**
+     * setter method for MTL
+     * @param MTL to be set
+     */
     public void setMTL(int MTL) {
         this.MTL = MTL;
     }
 
+    /**
+     * getter method for MTH
+     * @return Item's MTH
+     */
     public int getMTH() {
         return MTH;
     }
 
+    /**
+     * setter method for MTH
+     * @param MTH to be set
+     */
     public void setMTH(int MTH) {
         this.MTH = MTH;
     }
 
+    /**
+     * getter method for ords
+     * @return Item's ords
+     */
     public int getOrds() {
         return ords;
     }
 
+    /**
+     * setter method for ords
+     * @param ords to be set
+     */
     public void setOrds(int ords) {
         this.ords = ords;
     }
